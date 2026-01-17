414f7483fd532926e36fe22566a75e1d692fbf19
Your Name <maharramalverdiyev890@gmail.com>
2026-01-17 15:16:10 +0400

    SectionSixUptaded

--- src/pages/sectionSix/SectionSix.jsx
+++ src/pages/sectionSix/SectionSix.jsx
@@ -1,13 +1,41 @@
 import React from 'react'
+import '../sectionSix/sectionsix.css'
+
 
 const SectionSix = () => {
   return (
       <div className='sectionSix' >
             <h1>Tez-tez verilən suallar</h1>
             <div className="sectionSix-container">
-                <div className="card">
-                    <h6>Medbooking</h6>
-                    <p>Lorem impuls</p>
+                <div className="questions">
+                    <div className="questions-left">
+                        <div className="questions-text">
+                            <h6>ThreeSteps LLC hansı xidmət göstərir?</h6>
+                            <p>Biz İT infrastrukturunun qurulması, sistem inteqrasiyası, texniki dəstək, proqram təminatı, avtomatlaşdırma və süni intellekt əsaslı texnoloji həllər təqdim edirik.</p>
+                        </div><div className="questions-text">
+                            <h6>Layihə müddəti necə müəyyən olunur?</h6>
+                            <p>Layihənin həcmi, texniki tələblər və məqsədlər əsasında qiymətləndirmə aparılır.
+                                 Bundan sonra müştəri ilə razılaşdırılmış konkret vaxt planı təqdim olunur.</p>
+                        </div><div className="questions-text">
+                            <h6>Texnoloji tərəfdaşlıq necə həyata keçirilir?</h6>
+                            <p>Biz uzunmüddətli əməkdaşlığa üstünlük veririk. Yalnız bir layihəni deyil,
+                                 müştərinin bütün texnoloji inkişaf mərhələlərini dəstəkləyirik.</p>
+                        </div>
+                    </div>
+                    <div className="questions-right">
+                        <div className="questions-text">
+                            <h6>Xidmətlər yalnız korporativ müştərilər üçündürmü?</h6>
+                            <p>Xeyr, biz həm böyük şirkətlərlə, həm də kiçik və orta bizneslərlə 
+                                əməkdaşlıq edirik. Hər müştəri üçün fərdi həll təqdim edirik.</p>
+                        </div><div className="questions-text">
+                            <h6>Texniki dəstək göstərirsinizmi?</h6>
+                            <p>Bəli. ThreeSteps LLC 24/7 texniki dəstək və monitorinq xidməti təqdim edir ki, sistemləriniz daim sabit və təhlükəsiz işləsin.</p>
+                        </div><div className="questions-text">
+                            <h6>Xidmətlərdən necə faydalana bilərəm?</h6>
+                            <p>Sadəcə bizimlə əlaqə saxlayın, mütəxəssislərimiz biznes 
+                                ehtiyaclarınızı analiz edib, ən uyğun texnoloji həll paketini təklif edəcəklər.</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
